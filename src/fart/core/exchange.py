191a9c7b9e808be48cdf25fec8a432d745613500
Koen van Esterik <koen@vanesterik.dev>
2026-08-03 15:15:17 +0200

    Revert "style: ruff format/fix"
    This reverts commit 70869d25d23c8197076f4ef46afc12ebdde87770.

--- src/fart/core/exchange.py
+++ src/fart/core/exchange.py
@@ -71,36 +71,44 @@ class Exchange:
 
     @property
     def market(self) -> str:
+
         return self._market
 
     @market.setter
     def market(self, market: str) -> None:
+
         self._market = market
 
     @property
     def interval(self) -> str:
+
         return self._interval
 
     @interval.setter
     def interval(self, interval: str) -> None:
+
         self._interval = interval
 
     @property
     def balance(self) -> List[BalanceAsset]:
+
         balance_data = self._client.balance()
         return [BalanceAsset(**asset) for asset in balance_data]
 
     @property
     def last_candle(self) -> Candle:
+
         return self._get_candles(limit=1)[0]
 
     @property
     def price(self) -> Price:
+
         price_date = self._client.tickerPrice(options={"market": self._market})
         return Price(**price_date)
 
     @property
     def trades(self) -> List[Trade]:
+
         trades = self._client.trades(self._market)
         return [Trade(**trade) for trade in trades]
 
@@ -110,6 +118,7 @@ class Exchange:
         start: Optional[int] = None,
         end: Optional[int] = None,
     ) -> List[Candle]:
+
         candles: List[Candle] = self._client.candles(
             self._market,
             self._interval,
@@ -122,14 +131,17 @@ class Exchange:
         return candles
 
     def initiate(self, callback: Callable[[Dict[str, Any]], None]) -> None:
+
         self._socket = self._client.newWebsocket()
         self._socket.subscriptionCandles(self._market, self._interval, callback)
         self._socket.setErrorCallback(self._error_callback)
 
     def _error_callback(self, error: Any) -> None:
+
         print("Errors:", json.dumps(error, indent=2))
 
     def wait_and_close(self) -> None:
+
         # Bitvavo uses a weight-based rate limiting system. The application is
         # limited to 1000 weight points per IP or API key per minute. The rate
         # weighting for each endpoint is provided in the Bitvavo API
